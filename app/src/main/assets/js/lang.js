3df2b79988a1ad22d97a94590ca05c7b5691a6f1
a1foks1971 <a1foks1971@gmail.com>
2025-07-09 12:03:44 +0300

    clear some comments

--- app/src/main/assets/js/lang.js
+++ app/src/main/assets/js/lang.js
@@ -1,50 +1,22 @@
-//document.addEventListener('DOMContentLoaded', () => {
-//  const langSwitcher = document.getElementById('lang-switcher');
-////  let lang = 'us'; // язык по умолчанию
-//  let lang = 'uk'; // язык по умолчанию
-//
-//  function setLanguage(lang) {
-//    document.querySelector('[data-page="home"] #page-title').textContent   = strings.home.teamman[lang];
-//    document.querySelector('[data-page="home"] #btn-settings').textContent = strings.home.settings[lang];
-//    document.querySelector('[data-page="home"] #btn-next').textContent     = strings.home.next[lang];
-//  }
-//
-//  setLanguage(lang);
-//
-//  langSwitcher.addEventListener('change', (e) => {
-//    lang = e.target.value;
-//    setLanguage(lang);
-//  });
-//});
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const langSwitcher = document.getElementById('lang-switcher');
-  let lang = 'uk'; // язык по умолчанию
+  let lang = 'uk';
 
-//const page = document.body.dataset.page;
-//
+  const page = document.body.dataset.page;
+
   function setLanguage(lang) {
-//    switch (page) {
-//      case 'home':
-//        document.getElementById('page-title').textContent   = strings.home.teamman[lang];
-//        document.getElementById('btn-settings').textContent = strings.home.settings[lang];
-//        document.getElementById('btn-next').textContent     = strings.home.next[lang];
+    switch (page) {
+      case 'home':
         document.querySelector('[data-page="home"] #page-title').textContent   = strings.home.teamman[lang];
         document.querySelector('[data-page="home"] #btn-settings').textContent = strings.home.settings[lang];
         document.querySelector('[data-page="home"] #btn-next').textContent     = strings.home.next[lang];
-//        break;
-
-//      case 'settings':
-//        document.getElementById('page-title').textContent = strings.settings.settings[lang];
-//        document.getElementById('db-btn').textContent      = strings.settings.database[lang];
-//        document.getElementById('users-btn').textContent   = strings.settings.users[lang];
-//        break;
-
-      // сюда можно добавить другие страницы
-//    }
-//  }
-
+        break;
+      case 'settings':
+        document.getElementById('page-title').textContent = strings.settings.settings[lang];
+        document.getElementById('db-btn').textContent      = strings.settings.database[lang];
+        document.getElementById('users-btn').textContent   = strings.settings.users[lang];
+        break;
+    }
   }
 
   setLanguage(lang);
