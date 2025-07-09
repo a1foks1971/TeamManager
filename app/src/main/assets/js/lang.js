24e44bdb431e281a9fb84ac3baf239aa328215ed
a1foks1971 <a1foks1971@gmail.com>
2025-07-09 11:16:23 +0300

    Language toggling. it is maybe working

--- app/src/main/assets/js/lang.js
+++ app/src/main/assets/js/lang.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const langSwitcher = document.getElementById('lang-switcher');
-  let lang = 'us'; // язык по умолчанию
-//  let lang = 'uk'; // язык по умолчанию
+//  let lang = 'us'; // язык по умолчанию
+  let lang = 'uk'; // язык по умолчанию
 
   function setLanguage(lang) {
     document.querySelector('[data-page="home"] #page-title').textContent   = strings.home.teamman[lang];
-    document.querySelector('[data-page="home"] #settings-btn').textContent = strings.home.settings[lang];
-    document.querySelector('[data-page="home"] #next-btn').textContent     = strings.home.next[lang];
+    document.querySelector('[data-page="home"] #btn-settings').textContent = strings.home.settings[lang];
+    document.querySelector('[data-page="home"] #btn-next').textContent     = strings.home.next[lang];
   }
 
   setLanguage(lang);
